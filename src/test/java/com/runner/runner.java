48d8f1182cf805924e5508f9df2ebcff0e071602
login <jaga.jagatheeshjk@gmail.com>
2023-11-16 16:23:36 +0530

    update

--- src/test/java/com/runner/runner.java
+++ src/test/java/com/runner/runner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features= "src/test/resources/Features/admin.feature", glue="com.stepdefinition",strict=true,dryRun=false,monochrome=true,
+@CucumberOptions(features= "src/test/resources/Features/orange_login.feature", glue="com.stepdefinition",strict=true,dryRun=false,monochrome=true,
        plugin ="com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:")
 
 public class runner {
